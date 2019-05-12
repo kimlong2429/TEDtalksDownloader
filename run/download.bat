3cbe72fbb57630c45734e3ba9f78f10e042975ac
kimlong2429 <kimlong.2429@gmail.com>
2019-05-12 14:01:07 +0700

    update run command on windows

--- run/download.bat
+++ run/download.bat
@@ -1,2 +1,2 @@
 @echo off
-call java -jar E:\workspace\eclipse\home\TEDtalksDownloader\build\libs\Ted-1.0.0.jar %*
+call java -jar E:\workspace\eclipse\home\TEDtalksDownloader\build\libs\Ted-2.0.0.jar %*
